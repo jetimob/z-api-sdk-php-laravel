9f281ae383086e4ae74c6ae8c195a1df30c21816
Ricardo Rohrs <rerohrs@inf.ufsm.br>
2023-02-16 14:30:48 -0300

    feat: Cria mensagem com link

---
+++ src/Entity/MessageLink.php
@@ -0,0 +1,189 @@
+<?php
+
+namespace Jetimob\ZApi\Entity;
+
+class MessageLink extends Entity
+{
+    protected string $phone;
+    protected string $message;
+    protected string $image;
+    protected string $linkUrl;
+    protected string $title;
+    protected string $linkDescription;
+    protected ?int $delayMessage = null;
+    protected ?int $delayTyping = null;
+    protected ?string $linkType = null;
+
+    /**
+     * @return string
+     */
+    public function getPhone(): string
+    {
+        return $this->phone;
+    }
+
+    /**
+     * @param string $phone
+     * @return MessageLink
+     */
+    public function setPhone(string $phone): MessageLink
+    {
+        $this->phone = $phone;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getMessage(): string
+    {
+        return $this->message;
+    }
+
+    /**
+     * @param string $message
+     * @return MessageLink
+     */
+    public function setMessage(string $message): MessageLink
+    {
+        $this->message = $message;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getImage(): string
+    {
+        return $this->image;
+    }
+
+    /**
+     * @param string $image
+     * @return MessageLink
+     */
+    public function setImage(string $image): MessageLink
+    {
+        $this->image = $image;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLinkUrl(): string
+    {
+        return $this->linkUrl;
+    }
+
+    /**
+     * @param string $linkUrl
+     * @return MessageLink
+     */
+    public function setLinkUrl(string $linkUrl): MessageLink
+    {
+        $this->linkUrl = $linkUrl;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param string $title
+     * @return MessageLink
+     */
+    public function setTitle(string $title): MessageLink
+    {
+        $this->title = $title;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLinkDescription(): string
+    {
+        return $this->linkDescription;
+    }
+
+    /**
+     * @param string $linkDescription
+     * @return MessageLink
+     */
+    public function setLinkDescription(string $linkDescription): MessageLink
+    {
+        $this->linkDescription = $linkDescription;
+        return $this;
+    }
+
+    /**
+     * @return int|null
+     */
+    public function getDelayMessage(): ?int
+    {
+        return $this->delayMessage;
+    }
+
+    /**
+     * @param int|null $delayMessage
+     * @return MessageLink
+     */
+    public function setDelayMessage(?int $delayMessage): MessageLink
+    {
+        $this->delayMessage = $delayMessage;
+        return $this;
+    }
+
+    /**
+     * @return int|null
+     */
+    public function getDelayTyping(): ?int
+    {
+        return $this->delayTyping;
+    }
+
+    /**
+     * @param int|null $delayTyping
+     * @return MessageLink
+     */
+    public function setDelayTyping(?int $delayTyping): MessageLink
+    {
+        $this->delayTyping = $delayTyping;
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getLinkType(): ?string
+    {
+        return $this->linkType;
+    }
+
+    /**
+     * @param string|null $linkType
+     * @return MessageLink
+     */
+    public function setLinkType(?string $linkType): MessageLink
+    {
+        $this->linkType = $linkType;
+        return $this;
+    }
+
+    public static function new(string $phone, string $message, string $title, string $image, string $linkUrl, string $linkDescription): self
+    {
+        return (new static())
+            ->setPhone($phone)
+            ->setMessage($message)
+            ->setTitle($title)
+            ->setImage($image)
+            ->setLinkUrl($linkUrl)
+            ->setLinkDescription($linkDescription);
+    }
+}
--- tests/Feature/MessageApiTest.php
+++ tests/Feature/MessageApiTest.php
@@ -4,6 +4,7 @@ namespace Jetimob\ZApi\Tests\Feature;
 
 use Jetimob\ZApi\Api\Message\MessageApi;
 use Jetimob\ZApi\Entity\Message;
+use Jetimob\ZApi\Entity\MessageLink;
 use Jetimob\ZApi\Facade\ZApi;
 use Jetimob\ZApi\Tests\AbstractTestCase;
 
@@ -24,9 +25,25 @@ class MessageApiTest extends AbstractTestCase
     }
 
     /** @test */
-    public function sendMessageShouldSuccess(): void
+    public function sendTextMessageShouldSuccess(): void
     {
-        $response = $this->api->sendMessage(Message::new('5551996550359', 'teste da Z-API'));
+        $response = $this->api->sendTextMessage(Message::new('5551996550359', 'teste da Z-API'));
+        $this->assertSame(200, $response->getStatusCode());
+        dump($response);
+    }
+
+    /** @test */
+    public function sendTextWithLinkMessageShouldSuccess(): void
+    {
+        $response = $this->api->sendTextWithLinkMessage(MessageLink::new(
+            '5551996550359',
+            'teste da Z-API',
+            'TESTE',
+            'https://ac-landing-pages-user-uploads-production.s3.amazonaws.com/0000053730/ae361e8e-dbea-4a4f-a121-e0a04481de1d.png',
+            'https://app.jetimob.com/',
+            'teste de link z-api',
+        ));
+
         $this->assertSame(200, $response->getStatusCode());
         dump($response);
     }
